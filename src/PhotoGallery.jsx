b3868b751e0fba322282f861f2dc3b20c55e22a6
turnermanxi <cturnerelrey@gmail.com>
2024-09-26 03:36:39 -0500

    Added fullscreen viewr and photogallery functions for the gallery page, waiting for images

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -5,3 +5,44 @@ import './Photogallery.css'
 const importAll = (r) => r.keys().map(r);
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
+const Photogallery = () => {
+    const [currentIndex, setCurrentIndex] = useState(null);
+
+    const handleImageClick = (index) => {
+        setCurrentIndex(index);
+    };
+
+    const handleCloseViewer = () => {
+        setCurrentIndex(null);
+    }
+
+    return (
+        <>
+        <div className='photo-gallery'>
+            <div className='gallery-grid'>
+                {images.map((src, index) => (
+                    <img
+                    key={index}
+                    src={src}
+                    alt={`Gallery Image ${index + 1}`}
+                    className="gallery-image"
+                    onClick={() => handleImageClick(index)}
+                    />
+                ))}
+            </div>
+
+            {currentIndex !== null && (
+                <FullScreenViewer
+                images={images}
+                currentIndex={currentIndex}
+                onClose={handleCloseViewer}
+                setCurrentIndex={setCurrentIndex}
+                />
+                )}
+        </div>
+        </>
+    );
+};
+
+export default Photogallery;
+
